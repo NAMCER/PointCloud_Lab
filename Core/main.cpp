0b32be8d76ce79f88a01816d5a5c4e8cf5116e89
linanzhe <cnlnz@hotmail.com>
2020-12-17 23:28:05 +0800

    add core

---
+++ Core/main.cpp
@@ -0,0 +1,22 @@
+
+#include "myFunction.h"
+
+
+int main(int argc, char** argv)
+{
+	std::string filePath = R"(../Data/out.pcd)";
+	std::string outPath = R"(../Data/out.pcd)";
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+	pclfileIOStream::readCloudfile(filePath, *cloud);
+	baseFitting::calculateSphereCoef(cloud);
+
+	//makePointCloud(outPath);
+
+	std::cout << "Read Point size: " << cloud->size() << std::endl;
+
+	system("pause");
+
+	return (0);
+
+}
